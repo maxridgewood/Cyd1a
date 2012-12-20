4374e430a66616ad1dc2d8ae0cea4128da17d76e
Testing <testing@Testings-MacBook.local>
2012-12-20 00:19:16 -0500

    Initial Commit

---
+++ Cydia/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Cydia
+//
+//  Created by Testing on 12/20/12.
+//  Copyright (c) 2012 Testing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ABCAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ABCAppDelegate class]));
+    }
+}
---
+++ CydiaTests/CydiaTests.h
@@ -0,0 +1,13 @@
+//
+//  CydiaTests.h
+//  CydiaTests
+//
+//  Created by Testing on 12/20/12.
+//  Copyright (c) 2012 Testing. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CydiaTests : SenTestCase
+
+@end
